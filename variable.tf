491efe7d4cd190464fa14ba72917c74ed6c9744f
Aryakadam <kadam.arya9058@gmail.com>
2026-03-09 15:37:22 +0530

    data block

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "region-name" {
+  default = "ap-south-1"
+}
+
+variable "instance_name" {
+ default =  "t3.micro"
+}
+
+variable "image" {
+  default = "ami-051a31ab2f4d498f5"
+}
+
+variable "key-pair" {
+  default = "mumbai-key"
+}
