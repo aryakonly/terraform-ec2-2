cc983a8293fbbad8eb71de85a3b850849136559f
Aryakadam <kadam.arya9058@gmail.com>
2026-03-09 15:38:49 +0530

    data block

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "aws_instance" "name" {
   ami = var.image
   instance_type = var.instance_name
   key_name = var.key-pair
-  vpc_security_group_ids = [data.aws_security_group.sg]
+  vpc_security_group_ids = [data.aws_security_group.sg-1.name]
 }
-data "aws_security_group" "sg" {
+data "aws_security_group" "sg-1" {
   name = "sg"
   vpc_id = "vpc-0e8c799f8edc4f620 "
 }
