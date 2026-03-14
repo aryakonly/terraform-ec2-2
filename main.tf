a2319c7ce4d81deae315c3d5ff8a61f261ed0787
Aryakadam <kadam.arya9058@gmail.com>
2026-03-14 19:33:16 +0530

    userdata is added

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "name" {
   ami = var.image
   instance_type = var.instance_name
   key_name = var.key-pair
-  vpc_security_group_ids = ["sg-0002f6ecceaad3bdc"]
+  vpc_security_group_ids = var.sg
   user_data = base64encode(file("userdata.sh"))
   tags = {
     Name = "web-server"
--- variable.tf
+++ variable.tf
@@ -10,6 +10,10 @@ variable "image" {
   default = "ami-051a31ab2f4d498f5"
 }
 
+variable "sg" {
+  default = ["sg-0002f6ecceaad3bdc"]
+}
+
 variable "key-pair" {
   default = "mumbai-key"
 }
